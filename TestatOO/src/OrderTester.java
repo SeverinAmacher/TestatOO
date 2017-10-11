b3f52318acb0119f75bc1d4157d1e722e49b9b14
Julia Tanner <juliatanner@hotmail.com>
2017-10-11 10:39:51 +0200

    merge OrderTester

--- TestatOO/src/OrderTester.java
+++ TestatOO/src/OrderTester.java
@@ -1,19 +1,12 @@
 
 public class OrderTester {
 	public static void main(String[] args) {
-		ProductItem item1 = new ProductItem("Laptop XY");
-		ServiceItem item2 = new ServiceItem("Auto waschen");
-		ProductItem item3 = new ProductItem("Handy XXX");
-		ProductItem item4 = new ProductItem("Stromkabel");
-		ServiceItem item5 = new ServiceItem("Wohnung putzen");
-		Item[] items = new Item[] {item1, item2, item3, item4, item5};
 		ProductItem item1 = createProductItem("Laptop XY", 1, 1350);
 		ServiceItem item2 = createServcieItem("Auto waschen", 25);
 		ProductItem item3 = createProductItem("Handy XXX", 1, 250.95);
 		ProductItem item4 = createProductItem("Stromkabel", 3, 5.50);
 		ServiceItem item5 = createServcieItem("Wohnung putzen", 55);
 		
-		Order order = new Order(items);
 		Order order = new Order(new Item[] {item1, item2, item3, item4, item5});
 		order.printItems();		
 	}
