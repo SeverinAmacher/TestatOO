a1dd420cb7b310a7416bae1362024ae173748009
Julia Tanner <juliatanner@hotmail.com>
2017-10-11 10:17:37 +0200

    Item and Order class

--- TestatOO/src/Order.java
+++ TestatOO/src/Order.java
@@ -1,4 +1,24 @@
 
 public class Order {
 
+	private Item[] items;
+	
+	public Order(Item[] items) {
+		this.items = items;
+	}
+
+	public double getTotalPrice() {
+		double total = 0;
+		for(Item item : items) {
+			total += item.getPrice();
+		}
+		return total;
+	}
+	
+	public void printItems() {
+		System.out.println("Folgende Produkte sind in der Bestellung: ");
+		for(Item item : items) {
+			item.print();
+		}
+	}
 }
